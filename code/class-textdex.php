4ada05ba7d47d8b7a7d9cfdec34952b6bb3ed1b3
ollie <olliejones@gmail.com>
2024-03-22 17:49:46 -0400

    Trigram work in progress.

--- code/class-textdex.php
+++ code/class-textdex.php
@@ -3,9 +3,13 @@
 namespace Fast_Woo_Order_Lookup;
 
 class Textdex {
+	/**
+	 * Create the trigram table.
+	 *
+	 * @return void
+	 */
 	public function activate() {
 		global $wpdb;
-		delete_option( FAST_WOO_ORDER_LOOKUP_SLUG . 'textdex_status' ); //TODO
 		$tablename      = $wpdb->prefix . 'fast_woo_textdex';
 		$postmeta       = $wpdb->postmeta;
 		$ordersmeta     = $wpdb->prefix . 'wc_orders_meta';
@@ -13,24 +17,28 @@ class Textdex {
 			array(
 				'new'     => true,
 				'current' => 0,
-				'batch'   => 10,
+				'batch'   => 50,
 				'last'    => - 1
 			) );
 
 		if ( array_key_exists( 'new', $textdex_status ) ) {
-			$table = <<<TABLE
+			$table  = <<<TABLE
 CREATE TABLE $tablename (
     id BIGINT NOT NULL,
-    dir TINYINT NOT NULL DEFAULT 0,
-	word VARCHAR(128) NOT NULL,
-	PRIMARY KEY (dir, word, id)
+	trigram CHAR(3) NOT NULL,
+	PRIMARY KEY (trigram, id)
 );
 TABLE;
-			$wpdb->query( $table );
+			$result = $wpdb->query( $table );
+			if ( false === $result ) {
+				if ( ! str_contains( $wpdb->last_error, 'already exists' ) ) {
+					$wpdb->bail( 'Table creation failure ' );
+				}
+			}
 			unset ( $textdex_status['new'] );
 			$query                  = <<<QUERY
 			SELECT GREATEST (
-				(SELECT MAX(post_id) FROM $postmeta WHERE meta_key IN ('_billing_address_index','_shipping_address_index')),
+				(SELECT MAX(post_id) FROM $postmeta WHERE meta_key IN ('_billing_address_index','_shipping_address_index','_billing_last_name','_billing_email','_billing_phone')),
 				(SELECT MAX(id) FROM $ordersmeta WHERE meta_key IN ('_billing_address_index','_shipping_address_index'))			       
 			) id;
 
@@ -39,12 +47,17 @@ QUERY;
 			update_option( FAST_WOO_ORDER_LOOKUP_SLUG . 'textdex_status', $textdex_status, false );
 		}
 	}
+//('_billing_address_index','_shipping_address_index','_billing_last_name','_billing_email','_billing_phone')
 
-	public function doAllBatches() {
+	/**
+	 * @return void
+	 */
+	public function loadTextdex() {
 		global $wpdb;
 		$tablename  = $wpdb->prefix . 'fast_woo_textdex';
 		$postmeta   = $wpdb->postmeta;
 		$ordersmeta = $wpdb->prefix . 'wc_orders_meta';
+		$orderitems = $wpdb->prefix . 'woocommerce_order_items';
 
 		$done = false;
 		while( ! $done ) {
@@ -57,33 +70,103 @@ QUERY;
 			$last  = min( $first + $textdex_status['batch'], $textdex_status['last'] );
 
 			$query = <<<QUERY
-				SELECT DISTINCT * FROM (
+				SELECT id, GROUP_CONCAT(value SEPARATOR ' ') value
+				FROM (
 				SELECT post_id id, meta_value COLLATE utf8mb4_unicode_ci value
 				  FROM $postmeta
-				 WHERE meta_key IN ('_billing_address_index','_shipping_address_index')
+				 WHERE meta_key IN ('_billing_address_index','_shipping_address_index','_billing_last_name','_billing_email','_billing_phone')
 				   AND post_id >= %d and post_id <= %d
 				UNION ALL
 				SELECT id id, meta_value COLLATE utf8mb4_unicode_ci value
 				  FROM $ordersmeta
 				 WHERE meta_key IN ('_billing_address_index','_shipping_address_index')
 				   AND id >= %d and id < %d
-				) a;
+				UNION ALL
+				SELECT order_id id, order_item_name COLLATE utf8mb4_unicode_ci value
+				  FROM $orderitems
+				 WHERE order_id >= %d and order_id < %d
+				) a
+				GROUP BY id;
 QUERY;
 			$wpdb->query( 'BEGIN;' );
-			$query     = $wpdb->prepare( $query, array( $first, $last, $first, $last ) );
+			$query     = $wpdb->prepare( $query, array( $first, $last, $first, $last, $first, $last ) );
 			$resultset = $wpdb->get_results( $query );
+			if ( false === $resultset ) {
+				$wpdb->bail( 'Order data retrieval failure' );
+			}
+
 			foreach ( $resultset as $result ) {
 
-				foreach ( $this->textdex_entry( $result ) as $entry ) {
-					$wpdb->query( $wpdb->prepare(
-						"INSERT IGNORE INTO $tablename (id, dir, word ) VALUES (%d, %d, %s);",
-						$entry ) );
+				foreach ( $this->trigrams( $result->value ) as $trigram ) {
+					$query  = $wpdb->prepare(
+						"INSERT IGNORE INTO $tablename (id, trigram) VALUES (%d, %s);",
+						array( $result->id, $trigram ) );
+					$status = $wpdb->query( $query );
+
+					if ( false === $status ) {
+						$wpdb->bail( 'Trigram insertion failure' );
+					}
 				}
 			}
+
+			unset ( $resultset );
 			$wpdb->query( 'COMMIT;' );
-			$textdex_status['current'] = $last;
+			$textdex_status['current'] = $last + 1;
 			update_option( FAST_WOO_ORDER_LOOKUP_SLUG . 'textdex_status', $textdex_status, false );
 		}
+	}
+
+	/**
+	 * @return bool true if the trigram index is ready to use.
+	 */
+	public function isReady() {
+		$textdex_status = get_option( FAST_WOO_ORDER_LOOKUP_SLUG . 'textdex_status', array(
+			'new'     => true,
+			'current' => 0,
+			'batch'   => 50,
+			'last'    => - 1
+		) );
+
+		return $textdex_status['current'] > $textdex_status['last'];
+	}
+
+
+	public function trigrams( $value ) {
+
+		$result = array();
+		$value  = preg_replace( '/\s+/', ' ', trim( $value ) );
+		$len    = strlen( $value );
+		if ( $len <= 0 ) {
+			return array();
+		} else if ( 1 === $len ) {
+			$value .= '  ';
+		} else if ( 2 === $len ) {
+			$value .= ' ';
+		}
+		$len = strlen( $value ) - 2;
+		if ( $len > 0 ) {
+			for ( $i = 0; $i < $len; $i ++ ) {
+				$result [ substr( $value, $i, 3 ) ] = 1;
+			}
+		}
+
+		$result = array_keys( $result );
+		sort( $result );
+
+		return $result;
+	}
+
+	public function trigram_clause( $value ) {
+		global $wpdb;
+		$inlist   = array();
+		$trigrams = $this->trigrams( $value );
+		foreach ( $trigrams as $trigram ) {
+			$inlist[] = $wpdb->prepare( '%s', $trigram );
+		}
+		$clause = 'SELECT id FROM ' . $wpdb->prefix . 'fast_woo_textdex WHERE trigram IN (' . implode( ',', $inlist ) . ') ';
+		$clause .= $wpdb->prepare( 'GROUP BY id HAVING COUNT(*) = %d', count( $trigrams ) );
+
+		return $clause;
 	}
 
 	public function deactivate() {
@@ -94,36 +177,5 @@ QUERY;
 
 	}
 
-	private function textdex_entry( $result ) {
-
-		foreach ( $this->textdex_wordlist( $result->value ) as $word ) {
-			yield array( $result->id, 0, substr( $word, 0, 128 ) );
-			yield array( $result->id, 1, substr( strrev( $word ), 0, 128 ) );
-		}
-	}
-
-	private function textdex_wordlist( $value ) {
-		$patterns = array (
-			'/+?\.?\[d+/',
-		)
-		$nums         = array();
-		$numpattern   = '/[^-_\d\.\)\( ][-_\d\.\)\( ]+[^-_\d\.\)\( ]/';
-		$splitpattern = '/[^\w\d]+/';
-		preg_match_all( $numpattern, $value, $nums );
-/*		foreach ( $nums[0] as $num ) {
-			$num = trim( $num );
-			if ( strlen( $num ) > 0 ) {
-				$value = str_replace( $num, '', $value );
-				yield $num;
-				yield preg_replace( $splitpattern, '', $num );
-			}
-		}*/
-		foreach ( preg_split( $splitpattern, $value ) as $split ) {
-			$split = trim( $split );
-			if ( strlen( $split ) > 0 ) {
-				yield $split;
-			}
-		}
-	}
 }
 
--- fast-woo-order-lookup.php
+++ fast-woo-order-lookup.php
@@ -36,8 +36,10 @@ class FastWooOrderLookup {
 	private static $instance = null;
 
 	private $textdex;
+	private $filtering = false;
 
-	private $initialized = false;
+	private $term;
+	private $trigram_clause;
 
 	/**
 	 * Filter: Data store name.
@@ -76,14 +78,10 @@ class FastWooOrderLookup {
 		add_filter( 'woocommerce_shop_subscription_search_results', array( $this, 'filter_search_results' ), 10, 3 );
 
 		$dir = plugin_dir_path( __FILE__ );
-		$dir = '/home/ollie/src/fast-woo-order-lookup/';  //TODO
 		require_once( $dir . 'code/class-textdex.php' );
 		$this->textdex = new Textdex();
 		$this->textdex->activate();
-		$this->textdex->doAllBatches();
-
-
-		$this->initialized = true;
+		$this->textdex->loadTextdex();
 	}
 
 	/**
@@ -94,10 +92,11 @@ class FastWooOrderLookup {
 	 * @return array
 	 */
 	public function filter_search_fields( $search_fields ) {
-		if ( ! $this->initialized ) {
+		if ( ! $this->textdex->isReady() ) {
 			return $search_fields;
 		}
 		/* Hook to mung the queries. */
+		$this->filtering = true;
 		add_filter( 'query', array( $this, 'query' ), 1, 1 );
 
 		return $search_fields;
@@ -114,17 +113,19 @@ class FastWooOrderLookup {
 	 */
 	public function filter_search_results( $order_ids, $term, $search_fields ) {
 
-		/* Discontinue filtering queries after the metadata search */
-		remove_filter( 'query', array( $this, 'query' ), 1 );
+		if ( $this->filtering ) {
+			/* Discontinue filtering queries after the metadata search */
+			remove_filter( 'query', array( $this, 'query' ), 1 );
+			$this->filtering = false;
+		}
 
 		return $order_ids;
 	}
 
 	/**
-	 * Filters the database query.
+	 * Mung the order metadata search queries to include the trigram selection clause.
 	 *
-	 * Mung the order metadata search queries to change LIKE '%whatever%' to LIKE 'whatever%'
-	 * to make them sargable.
+	 * This is a MISERABLE hack. There aren't any suitable hooks to do this more elegantly.
 	 *
 	 * @param string $query Database query.
 	 *
@@ -132,10 +133,28 @@ class FastWooOrderLookup {
 	 */
 	public function query( $query ) {
 
-		return str_replace(
-			array( '.meta_value LIKE \'%', 'WHERE order_item_name LIKE \'%', '.user_mail LIKE \'%' ),
-			array( '.meta_value LIKE \'', 'WHERE order_item_name LIKE \'', '.user_mail LIKE \'' ),
-			$query );
+		if ( ! $this->term ) {
+			$splits = explode( 'LIKE \'%', $query, 2 );
+			if ( 2 !== count( $splits ) ) {
+				return $query;
+			}
+			$splits = explode( '%\'', $splits[1] );
+			if ( 2 !== count( $splits ) ) {
+				return $query;
+			}
+			$this->term           = $splits[0];
+			$this->trigram_clause = $this->textdex->trigram_clause( $this->term );
+		}
+		$newQuery = $query;
+		if ( str_contains( $newQuery, 'woocommerce_order_items as order_item' ) ) {
+			$newQuery = str_replace( 'WHERE order_item_name LIKE', 'WHERE order_id IN (' . $this->trigram_clause . ') AND order_item_name LIKE', $newQuery );
+		} else if ( str_contains( $newQuery , 'SELECT DISTINCT os.order_id FROM wp_wc_order_stats os') ) {
+			/* empty, intentionally */
+		} else {
+			$newQuery = str_replace( 'postmeta p1 WHERE ', 'postmeta p1 WHERE post_id IN (' . $this->trigram_clause . ') AND ', $newQuery );
+		}
+
+		return $newQuery;
 	}
 }
 
@@ -175,7 +194,7 @@ function activate() {
 
 function deactivate() {
 
-	require_once( plugin_dir_path( __FILE__ ) . 'code/class-textdex.php.' );
+	require_once( plugin_dir_path( __FILE__ ) . 'code/class-textdex.php' );
 	$textdex = new Textdex();
 	$textdex->deactivate();
 }
